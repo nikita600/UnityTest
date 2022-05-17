cdd3c409cdde9791fa3185dfe9572e87eece9459
nikita600 <nikitak600@gmail.com>
2022-05-18 02:29:07 +0300

    Add Enemy shooting mechanic.

--- Assets/Scripts/Units/EnemyController.cs
+++ Assets/Scripts/Units/EnemyController.cs
@@ -1,7 +1,24 @@
-﻿namespace Game
+﻿using Units.Weapon;
+using UnityEngine;
+
+namespace Game
 {
-    public class EnemyController : UnitController
+    public sealed class EnemyController : UnitController
     {
+        [SerializeField]
+        private WeaponController _weaponController = null;
+
+        [SerializeField]
+        private EyesSensorController _eyesSensorController = null;
+        
+        private void FixedUpdate()
+        {
+            if (_eyesSensorController.IsTargetVisible)
+            {
+                _weaponController.Fire();
+            }
+        }
+
         protected override void OnDeadInternal()
         {
             base.OnDeadInternal();
---
+++ Assets/Scripts/Units/EyesSensorController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Utilities;
+
+namespace Game
+{
+    public class EyesSensorController : MonoBehaviour
+    {
+        [SerializeField]
+        private ColliderTagSettings _colliderTagSettings = null;
+
+        [SerializeField]
+        private bool _playerTarget = false;
+
+        public bool IsTargetVisible { get; private set; } = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            IsTargetVisible = _playerTarget 
+                ? _colliderTagSettings.IsPlayerCollider(other) 
+                : _colliderTagSettings.IsEnemyCollider(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            IsTargetVisible = !(_playerTarget 
+                ? _colliderTagSettings.IsPlayerCollider(other) 
+                : _colliderTagSettings.IsEnemyCollider(other));
+        }
+    }
+}
